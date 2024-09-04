fbfca5f3d5eded0abe615ae230012bde5090bb8d
Ayman-dev1 <aymanelwan67@gmail.com>
2024-09-04 15:46:48 +0300

    updates on wizard

--- models/patient.py
+++ models/patient.py
@@ -6,16 +6,22 @@ class Patient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Name', required=True)
-    age = fields.Integer(string='Age')
+    age = fields.Integer(string='Age', required=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
     note = fields.Text(string='Notes')
-    examination_date = fields.Date(string='Examination Date')
+    examination_date = fields.Date(string='Examination Date', compute='_compute_examination_date', store=True)
     department_id = fields.Many2one('department', string='Department')
     doctor_id = fields.Many2one('doctor', string='Doctor')
     state = fields.Selection(
         [('after', 'After'), ('examination', 'Examination'), ('done', 'Done')],
         default='after'
     )
+
+    @api.depends('state')
+    def _compute_examination_date(self):
+        for record in self:
+            if record.state == 'examination':
+                record.examination_date = fields.Date.today()
 
     @api.onchange('department_id')
     def _onchange_department_id(self):
